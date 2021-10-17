88399343849dc31b6073d7eb71f36fae9caa6f6c
tatianacb5 <88335071+tatianacb5@users.noreply.github.com>
2021-10-17 20:07:52 -0300

    entrega5

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,4 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+
 
 const JSON_OTRO = 'https://japdevdep.github.io/ecommerce-api/cart/654.json'
 
